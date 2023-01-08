eabed175a7b2b099a8e71f20177b962615cbc9e6
Usman <usmanathhar.waxsys@gmail.com>
2023-01-08 17:00:51 +0500

    versoin_1.0

--- snp_pline/mainapp/urls.py
+++ snp_pline/mainapp/urls.py
@@ -6,6 +6,7 @@ urlpatterns=[
 
     path('vardataretrieval/vardataretrievalprocessing', views.vardataretrievalprocessing, name='vdataretproc'),
     path('runtools', views.runTools, name='runtools'),
+    path('runtools/annotatevariants', views.annotateVariants, name='annotate_variants'),
     path('vardataretrieval', views.varDataRetrieval, name='vardataretrieval'),
     path('vardataretrieval/download', views.downloadRSIdFile, name="downloadRSId"),
     path('vardataretrieval/download_grch37data', views.downloadGRCh37File, name="downloadGRCh37"),
--- snp_pline/mainapp/views.py
+++ snp_pline/mainapp/views.py
@@ -12,6 +12,7 @@ from .csvwriting import CSVFileWriting
 from .proteinvariantdata import ProteinVariantData
 from .refseqid_to_uniprotid import RefSeqId_to_UniProtId
 import uuid
+import myvariant
 import numpy as np
 # Create your views here.
 noOk=0
@@ -65,6 +66,9 @@ def vardataretrievalprocessing(request):
                 prot_var_data.parsevardatabystring(dbsnpvardata_str)
             if chekboxValues.__contains__('rsids'):
                 id = 'rs' + str(varid)
+                #query='http://www.mulinlab.org/vportal/portal/api?q=' + id + '&g=hg19&p=eur&f=pathogenicity'
+                #query='https://myvariant.info/v1/variant/' + id
+
                 rsIds.append(id)
 
             count = count + 1
@@ -88,13 +92,13 @@ def vardataretrievalprocessing(request):
             prot_var_data_filename = 'media/' + str(uuid.uuid4()) +'_protein_data' + '.txt'
             fw = FileWriting()
             fw.writeProteinData(protData,prot_var_data.prot_var_data_dict,prot_var_data_filename)
-
             protDataB=True
         if chekboxValues.__contains__('rsids'):
             fw = FileWriting()
             global rsIds_filename
             rsIds_filename = 'media/' + str(uuid.uuid4()) + '_rsids' + '.txt'
             fw.writeFileFromList(rsIds, rsIds_filename)
+
             rsIdsB=True
     context = {
 
@@ -104,6 +108,81 @@ def vardataretrievalprocessing(request):
         'grch38B':grch38B,
         'rsIdsB':rsIdsB,
         'protDataB':protDataB
+
+       }
+    return render(request, 'rsids.html', context)
+
+#annotate variants using myvariantinfo restful api
+def annotateVariants(request):
+    mv = myvariant.MyVariantInfo()
+    mvVar = ''
+    if request.method == 'POST':
+        givenTerm = request.POST.get('genId')
+        fabricatedTerm = givenTerm + ' AND missense variant[Function_Class]'
+        Entrez.email = "usman.athar@gmail.com"
+        handle = Entrez.esearch(db="snp", term=fabricatedTerm, retmax=5)
+        variantData = Entrez.read(handle)
+        totalSNPs = variantData['Count']
+        varids = variantData["IdList"]
+        chekboxValues = str(request.POST.getlist('chekboxValues', ''))
+        rsIds = []
+        var=''
+        print('chekboxValues=== ', chekboxValues)
+        for varid in varids:
+            id = 'rs' + str(varid)
+            var=mv.getvariant(id)
+
+            if chekboxValues.__contains__('sift'):  #writing GRCh37 chromosome coordinates
+                #mv.getvariant(id, fields=['cadd.sift.cat', 'cadd.sift.val'])
+                #fld=mv.get_fields('cadd.sift.cat')
+                print('fld=== ', mv.getvariant(id, fields=['dbnsfp.cadd,dbnsfp.cadd.pred', 'dbnsfp.cadd.raw_rankscore', 'dbnsfp.cadd.raw_score',
+                                                           'dbnsfp.sift,dbnsfp.sift.pred', 'dbnsfp.sift.converted_rankscore', 'dbnsfp.sift.score',
+                                                           'dbnsfp.sift4g,dbnsfp.sift4g.pred', 'dbnsfp.sift4g.converted_rankscore', 'dbnsfp.sift4g.score',
+                                                           'dbnsfp.provean,dbnsfp.provean.pred','dbnsfp.provean.rankscore', 'dbnsfp.provean.score',
+                                                           'dbnsfp.primateai,dbnsfp.primateai.pred','dbnsfp.primateai.rankscore', 'dbnsfp.primateai.score',
+                                                           'dbnsfp.polyphen2.hdiv,dbnsfp.polyphen2.hdiv.pred','dbnsfp.polyphen2.hdiv.rankscore', 'dbnsfp.polyphen2.hdiv.score',
+                                                           'dbnsfp.polyphen2.hvar', 'dbnsfp.polyphen2.hvar.pred', 'dbnsfp.polyphen2.hvar.rankscore', 'dbnsfp.polyphen2.hvar.score',
+                                                           'dbnsfp.revel','dbnsfp.revel.rankscore', 'dbnsfp.revel.score',
+                                                           'dbnsfp.mvp', 'dbnsfp.mvp.rankscore', 'dbnsfp.mvp.score',
+                                                           'dbnsfp.mpc', 'dbnsfp.mpc.rankscore', 'dbnsfp.mpc.score',
+                                                           'dbnsfp.mutpred,dbnsfp.mutpred.pred','dbnsfp.mutpred.rankscore', 'dbnsfp.mutpred.score',
+                                                           'dbnsfp.mutationtaster,dbnsfp.mutationtaster.pred','dbnsfp.mutationtaster.converted_rankscore', 'dbnsfp.mutationtaster.score',
+                                                           'dbnsfp.mutationassessor,dbnsfp.mutationassessor.pred','dbnsfp.mutationassessor.rankscore', 'dbnsfp.mutationassessor.score',
+                                                           'dbnsfp.metarnn,dbnsfp.metarnn.pred','dbnsfp.metarnn.rankscore', 'dbnsfp.metarnn.score',
+                                                           'dbnsfp.metasvm,dbnsfp.metasvm.pred','dbnsfp.metasvm.rankscore', 'dbnsfp.metasvm.score',
+                                                           'dbnsfp.metalr,dbnsfp.metalr.pred','dbnsfp.metalr.rankscore', 'dbnsfp.metalr.score',
+                                                           'dbnsfp.m-cap,dbnsfp.m-cap.pred','dbnsfp.m-cap.rankscore', 'dbnsfp.m-cap.score',
+                                                           'dbnsfp.lrt,dbnsfp.lrt.pred','dbnsfp.lrt.converted_rankscore', 'dbnsfp.lrt.score',
+                                                           'dbnsfp.list-s2,dbnsfp.list-s2.pred','dbnsfp.list-s2.rankscore', 'dbnsfp.list-s2.score',
+                                                           'dbnsfp.linsight','dbnsfp.linsight.rankscore', 'dbnsfp.linsight.score',
+                                                           'dbnsfp.gerp++,','dbnsfp.gerp++.rs', 'dbnsfp.gerp++.score',
+                                                           'dbnsfp.fathmm,dbnsfp.fathmm.pred','dbnsfp.fathmm.rankscore','dbnsfp.fathmm.score',
+                                                           'dbnsfp.fathmm-mkl,dbnsfp.fathmm-mkl.coding_pred','dbnsfp.fathmm-mkl.coding_rankscore', 'dbnsfp.fathmm-mkl.coding_score',
+                                                           'dbnsfp.fathmm-xf,dbnsfp.fathmm-xf.coding_pred','dbnsfp.fathmm-xf.coding_rankscore', 'dbnsfp.fathmm-xf.coding_score',]))
+
+            if chekboxValues.__contains__('polyphen'): # writing GRCh38 chromosome coordinates in csv format
+                mv.getvariant(id, fields=['cadd.sift.cat', 'cadd.sift.val'])
+            if chekboxValues.__contains__('provean'):
+                mv.getvariant(id, fields=['cadd.sift.cat', 'cadd.sift.val'])
+            if chekboxValues.__contains__('cadd'):
+                id = 'rs' + str(varid)
+
+                mv = myvariant.MyVariantInfo()
+                mvVar=mv.getvariant(id,fields=['cadd.sift.cat','cadd.sift.val'])
+                #siftFields=mvVar.get_fields("sift")
+
+                print('mvVar=== ',mvVar)
+
+
+
+
+            rsIdsB=True
+    context = {
+
+        'gene': givenTerm,
+        'totalSNPs':totalSNPs,
+
+
 
        }
     return render(request, 'rsids.html', context)
